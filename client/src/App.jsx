71a3ee937f8bf0a67e78a360cb86ec666c070983
Yord <yordwow@gmail.com>
2024-08-14 03:04:40 -0700

    Somthing Changinggggg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,13 @@ import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      {/* Header */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="bg-slate-300">
+      <div className="">
+        <h1 className="font-bold">Auth App</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Sign In</li>
+          <li>Home</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
