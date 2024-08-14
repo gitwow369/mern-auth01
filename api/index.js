af9321eb6e460f4da04fd68507897f6f36e2056e
Yord <yordwow@gmail.com>
2024-08-14 05:13:33 -0700

    Connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is Runnig your First Auth Project in PORT:3000");
 });
+// KLmBlNcdQEqCIclL
+// franklinyoutx
+//mernwow
+//123456Mern
