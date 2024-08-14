e2cc1c17cf3de59b714736e2115737394b36ccd3
Yord <yordwow@gmail.com>
2024-08-14 08:29:28 -0700

    create API Controllers Route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import userRoutes from "./routes/user.routes.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -17,6 +17,15 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is Runnig your First Auth Project in PORT:3000");
 });
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API is Working Yo",
+  });
+});
+
+app.use("/api/user", userRoutes);
+
 // KLmBlNcdQEqCIclL
 // franklinyoutx
 //mernwow
