1f63783bb721df229ed0afc0c4480ceea2e3bfdc
Yord <yordwow@gmail.com>
2024-08-14 20:05:34 -0700

    add a Midle ware and add A function to handle Errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   // console.log(req.body);
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -14,6 +14,7 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "User Created Sucessfully" });
   } catch (error) {
-    res.status(500).json(error.message);
+    // res.status(500).json(error.message);
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -30,6 +30,15 @@ app.get("/", (req, res) => {
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
+
 // KLmBlNcdQEqCIclL
 // franklinyoutx
 //mernwow
